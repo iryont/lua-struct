9bb59a10a6d1a0eb09194889f38b5d571eff3c9d
healLV <heal6@inbox.lv>
2019-07-02 04:22:07 +0300

    updated year

--- struct.lua
+++ struct.lua
@@ -1,5 +1,5 @@
 --[[
- * Copyright (c) 2015-2017 Iryont <https://github.com/iryont/lua-struct>
+ * Copyright (c) 2015-2019 Iryont <https://github.com/iryont/lua-struct>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
